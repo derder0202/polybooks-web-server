96b5b129c55a36fb8216470b61c30a1f54a7292a
der <mr73367@gmail.com>
2023-06-22 02:02:40 +0700

    der

--- api_src/controller/author_controller.js
+++ api_src/controller/author_controller.js
@@ -1,6 +1,6 @@
 const {Author} = require("../model/model");
 const authorController = {
-  // Get all authors
+    // Get all authors
     getAllAuthors: async (req, res) => {
         try {
             const startIndex = parseInt(req.query.startIndex) || 0;
@@ -8,69 +8,70 @@ const authorController = {
             const authors = await Author.find({})
                 .skip(startIndex)
                 .limit(limit);
+            //test
             res.status(200).json(authors);
         } catch (error) {
             console.error(error);
-            res.status(500).json({ error: 'Error getting authors from database.' });
+            res.status(500).json({error: 'Error getting authors from database.'});
         }
     },
 
-  // Get author by id
-  getAuthorById: async (req, res) => {
-    try {
-      const author = await Author.findById(req.params.id);
-      if (!author) {
-        res.status(404).json({ error: 'Author not found.' });
-      } else {
-        res.status(200).json(author);
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Error getting author from database.' });
-    }
-  },
+    // Get author by id
+    getAuthorById: async (req, res) => {
+        try {
+            const author = await Author.findById(req.params.id);
+            if (!author) {
+                res.status(404).json({error: 'Author not found.'});
+            } else {
+                res.status(200).json(author);
+            }
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({error: 'Error getting author from database.'});
+        }
+    },
 
-  // Create new author
-  createAuthor: async (req, res) => {
-    try {
-      const author = new Author(req.body);
-      const newAuthor = await author.save();
-      res.status(201).json(newAuthor);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Error creating author in database.' });
-    }
-  },
+    // Create new author
+    createAuthor: async (req, res) => {
+        try {
+            const author = new Author(req.body);
+            const newAuthor = await author.save();
+            res.status(201).json(newAuthor);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({error: 'Error creating author in database.'});
+        }
+    },
 
-  // Update author by id
-  updateAuthorById: async (req, res) => {
-    try {
-      const author = await Author.findByIdAndUpdate(req.params.id, req.body, { new: true });
-      if (!author) {
-        res.status(404).json({ error: 'Author not found.' });
-      } else {
-        res.status(200).json(author);
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Error updating author in database.' });
-    }
-  },
+    // Update author by id
+    updateAuthorById: async (req, res) => {
+        try {
+            const author = await Author.findByIdAndUpdate(req.params.id, req.body, {new: true});
+            if (!author) {
+                res.status(404).json({error: 'Author not found.'});
+            } else {
+                res.status(200).json(author);
+            }
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({error: 'Error updating author in database.'});
+        }
+    },
 
-  // Delete author by id
-  deleteAuthorById: async (req, res) => {
-    try {
-      const author = await Author.findByIdAndDelete(req.params.id);
-      if (!author) {
-        res.status(404).json({ error: 'Author not found.' });
-      } else {
-        res.status(204).json();
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Error deleting author from database.' });
+    // Delete author by id
+    deleteAuthorById: async (req, res) => {
+        try {
+            const author = await Author.findByIdAndDelete(req.params.id);
+            if (!author) {
+                res.status(404).json({error: 'Author not found.'});
+            } else {
+                res.status(204).json();
+            }
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({error: 'Error deleting author from database.'});
+        }
     }
-  }
 };
 
 module.exports = authorController
--- app.js
+++ app.js
@@ -48,6 +48,7 @@ app.set('view engine', 'hbs');
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(cors())
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
