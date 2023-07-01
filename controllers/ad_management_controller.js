c64e39eae425eda84de66243f327d0b41136a364
Sangnn <sangnnph14292@fpt.edu.vn>
2023-07-01 20:24:42 +0700

    update_1_7

--- controllers/ad_management_controller.js
+++ controllers/ad_management_controller.js
@@ -3,6 +3,9 @@ const adManagementController = {
         // cần list đầy đủ của member thường
         res.render('advertisement/ad_management');
     },
-    
+    detailManagement: async (req,res)=>{
+        // cần list đầy đủ của member thường
+        res.render('advertisement/banner_details');
+    },
 }
 module.exports = adManagementController
--- routes/admanagement.js
+++ routes/admanagement.js
@@ -3,6 +3,7 @@ const adManagementController = require("../controllers/ad_management_controller"
 var router = express.Router();
 
 router.get('/',adManagementController.listManagement)
+router.get('/banner_details',adManagementController.detailManagement)
 
 
 module.exports = router;
