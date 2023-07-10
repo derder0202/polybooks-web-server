ee36188ecd26d96d549769d21d9e9bf5eb98713c
Sangnn <sangnnph14292@fpt.edu.vn>
2023-07-10 21:52:12 +0700

    chuc_nang_duyet_daugia_10_7

--- routes/autionapproval.js
+++ routes/autionapproval.js
@@ -5,5 +5,6 @@ var router = express.Router();
 router.get('/',autionApprovalController.listAutionApproval); //duyệt bài đấu giá
 
 router.get('/detail_aution/:id',autionApprovalController.detailAution); //chi tiết duyệt bài đấu giá
+router.post('/detail_aution/:id',autionApprovalController.browseAuction);
 
 module.exports = router;
